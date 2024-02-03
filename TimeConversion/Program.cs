b3b37a48df5fbf4e105d2d879fe1739990b0319e
necati mert erlus <mert.erlus@outlook.com>
2024-02-03 16:43:18 +0300

    Add project files.

---
+++ TimeConversion/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'timeConversion' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static string timeConversion(string s)
+    {
+        string value = string.Empty;
+        DateTime date = Convert.ToDateTime(s);
+        value = date.ToString("HH:mm:ss");
+
+        return value;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+
+        string result = Result.timeConversion(s);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
